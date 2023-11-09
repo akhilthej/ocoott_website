e61de4abf9dac36b7139376d1f5137c25eab97b2
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-09 18:42:13 +0530

    login restrict

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
-import { Navbar, Footer, PrivacyPolicy, TermsnConditions, Disclaimer, ReturnPolicy, Error404, Home, Aboutus, Contactus } from './routes/Routes';
+import React, { useEffect } from 'react';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Navbar,
+  Footer,
+  PrivacyPolicy,
+  TermsnConditions,
+  Disclaimer,
+  ReturnPolicy,
+  Error404,
+  Home,
+  Aboutus,
+  Contactus,
+} from './routes/Routes';
 
-import {Telugu,Hindi} from './routes/Routes';
+import { Telugu, Hindi } from './routes/Routes';
 
 import Signup from './routes/UserData/SignupForm';
-import Login from './routes/UserData/Login'
+import Login from './routes/UserData/Login';
 
 import Player from './components/Players/Player';
-import PlayerFullScreen from './components/Players/PlayerFullScreen'
-
+import PlayerFullScreen from './components/Players/PlayerFullScreen';
 import OCOPlayer from './components/Players/OCOPlayer';
-
-import ShhhhQmovie from './routes/Originals/ShhhhQ'
-
+import ShhhhQmovie from './routes/Originals/ShhhhQ';
 
 export function App() {
   const location = useLocation();
+  const navigate = useNavigate(); // Fix: Corrected the import here
   window.scrollTo(0, 0);
 
+  // Mock authentication status, replace this with actual authentication logic
+  const isAuthenticated = localStorage.getItem('isLoggedIn') === 'true';
+
+  useEffect(() => {
+    // Redirect to /login if trying to access protected routes without login
+    const protectedRoutes = ['/telugu-movies', '/originals-movies'];
+    if (!isAuthenticated && protectedRoutes.includes(location.pathname)) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, location.pathname, navigate]);
 
   return (
     <>
@@ -27,8 +46,7 @@ export function App() {
 
       <Routes location={location} key={location.pathname}>
         <Route path="/" element={<Home />} />
-        <Route path="/telugu-movies" element={<Telugu />} />
-        <Route path="/originals-movies" element={<Hindi />} />
+   
 
         <Route path="/aboutus" element={<Aboutus />} />
         <Route path="/contactus" element={<Contactus />} />
@@ -48,6 +66,15 @@ export function App() {
         <Route path="/ShhhhQmovie" element={<ShhhhQmovie />} />
         
 
+        <Route path="/ShhhhQmovie" element={<ShhhhQmovie />} />
+        <Route
+          path="/telugu-movies"
+          element={isAuthenticated ? <Telugu /> : navigate('/login')} // Fix: Corrected the usage here
+        />
+        <Route
+          path="/originals-movies"
+          element={isAuthenticated ? <Hindi /> : navigate('/login')} // Fix: Corrected the usage here
+        />
 
         <Route path="*" element={<Error404 />} />
       </Routes>
