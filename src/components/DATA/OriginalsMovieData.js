523828107010136ed92be0a1c628f6f977cf106a
akhilthej <67720804+akhilthej@users.noreply.github.com>
2023-11-09 13:22:46 +0530

    23

--- src/components/DATA/OriginalsMovieData.js
+++ src/components/DATA/OriginalsMovieData.js
@@ -3,7 +3,7 @@ const OriginalsMovieData = [
       id: 'ORG001',
       name: "Shhhh'Q",
       thumbnail: 'https://drive.google.com/uc?id=1LooTfIqQLNoFlaIG_PFnfTJTakRNSDu4',
-      link: 'https://srv878-files.hstgr.io/2fc1f47d664e6b53/files/public_html/Movies/Youtubetestvideo.mp4',
+      link: 'https://rr4---sn-npoe7nsd.c.drive.google.com/videoplayback?expire=1699525020&ei=bIdMZZC_Lqibp84P9riVgAE&ip=2405:201:c023:8129:f143:d525:bd93:5fbb&id=02e8d1ce8e01b2f1&itag=22&source=webdrive&requiressl=yes&xpc=EghotM6WJ3oBAQ==&ttl=transient&susc=dr&driveid=1WS_bE1pcSaMEu6ZUtuJXuornyh-nVvTf&app=explorer&eaua=rbtpvI8GoUg&mime=video/mp4&vprv=1&prv=1&dur=4251.387&lmt=1699514071282584&subapp=DRIVE_WEB_FILE_VIEWER&txp=0006224&sparams=expire,ei,ip,id,itag,source,requiressl,xpc,ttl,susc,driveid,app,eaua,mime,vprv,prv,dur,lmt&sig=ANLwegAwRAIgOQDkydZZ-3bSvTvkNea6wgniKccmBboTDI4NskBXNIECIHhGFu-tPZEpqRAtvzBNOEULvzjOabEwZVnBZO52X2f8&cpn=HdG7wrlC3j4C_rFs&c=WEB_EMBEDDED_PLAYER&cver=1.20231105.00.01&redirect_counter=1&cm2rm=sn-h55lz7s&fexp=24350045&req_id=a6b90e8dbeeca3ee&cms_redirect=yes&cmsv=e&mh=oE&mm=34&mn=sn-npoe7nsd&ms=ltu&mt=1699513722&mv=m&mvi=4&pl=47&lsparams=mh,mm,mn,ms,mv,mvi,pl&lsig=AM8Gb2swRQIhAPjsQQeoJEo3eJcHcpGplBDR-z3435T1y_vulQ7wKOgZAiA3dSf79t5hwk1u7f-JTSSMqCB045dRuE-Nc0ghIfHsFg%3D%3D',
       link: 'shuu SENT.mp4 - Google Drive',
       director: 'Anudeep',
       gener: 'Love',
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import footer_csd from './csdv2Logofooter.png';
+import ocologo_footer from './OCOLogofooter.png';
+import csdlogo_footer from './csdv2Logofooter.svg'
 
 const Footer = () => {
   return (
@@ -36,84 +37,95 @@ const Footer = () => {
         </div>
       </section>*/}
 
-      {/* Footer */}
-      <section className="bg-black pb-5">
-        <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-9 lg:px-8">
-          <div className="grid grid-cols-1 gap-8 lg:grid-cols-3">
-            <div>
-              <Link to="/">
-                <img
-                  src={footer_csd}
-                  className="mr-5 h-10 sm:h-10"
-                  alt="footer_csd"
-                  width="200"
-                  height="200"
-                />
-              </Link>
-              <p className="max-w-xs mt-4 text-sm text-gray-600">
-                We are a web development and design company with a mission to help businesses establish an online presence.
-                We achieve this by continually developing technology, offering expert support, and ensuring a seamless online experience.
-              </p>
-              <div className="flex space-x-6 text-gray-600 pt-2">
-                <a href="https://www.facebook.com/profile.php?id=100087441512479" target="_blank" rel="noreferrer">
-                  <span className="sr-only">Facebook</span>
-                  <i className="fab fa-facebook"></i>
-                </a>
-                <a href="https://www.instagram.com/cyber.space.digital/" target="_blank" rel="noreferrer">
-                  <span className="sr-only">Instagram</span>
-                  <i className="fab fa-instagram"></i>
-                </a>
-                <a href="https://twitter.com/CyberSD1995" target="_blank" rel="noreferrer">
-                  <span className="sr-only">Twitter</span>
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="https://github.com/akhilthej" target="_blank" rel="noreferrer">
-                  <span className="sr-only">GitHub</span>
-                  <i className="fab fa-github"></i>
-                </a>
-              </div>
-            </div>
-            <div className="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
-              <div>
-                <div>
-                  <p className="font-medium text-white">Helpful Links</p>
-                  <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-                    <Link to="/contactus">Contact</Link>
-                    <Link to="/contactus">FAQs</Link>
-                    <a href="https://api.whatsapp.com/send?phone=918143407758&text=Welcome%20to%20Cyberspacedigital">Live Chat</a>
-                  </nav>
-                </div>
-              </div>
-              <div>
-                <p className="font-medium text-white">Legal</p>
-                <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
-                  <Link to="/privacypolicy">Privacy Policy</Link>
-                  <Link to="/terms&conditions">Terms &amp; Conditions</Link>
-                  <Link to="/returnpolicy">Cancellation & Refund Policy</Link>
-                  <Link to="/disclaimer">Disclaimer</Link>
-                  <Link to="/shipping&delivery">Shipping & Delivery</Link>
-                </nav>
-              </div>
-            </div>
-          </div>
-          <hr className="h-px my-8 border-0 bg-gray-700" />
-          <p className="cursor-default text-center text-xs text-white">
-            2023 © OTT.in <br/>{/*Designed and Developed by CyberSpaceDigital(www.cyberspacedigital.in) */}
-            <div className="flex pt-5 items-center justify-center">
-  <a href="www.cyberspacedigital.in">
-    <img
-      src={footer_csd}
-      className="h-10 sm:h-10"
-      alt="footer_csd"
-      width="200"
-      height="200"
-    />
-  </a> 
-</div>
 
-          </p>
+{/* Footer */}
+<section className="bg-black pb-5">
+  <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-9 lg:px-8">
+    <div className="grid grid-cols-1 gap-8 lg:grid-cols-4 lg:justify-items-center">
+      {/* 1st Column - Logo and Description */}
+      <div className="text-center">
+        <Link to="/">
+          <img
+            src={ocologo_footer}
+            className="mx-auto mb-2 h-10 sm:h-10"
+            alt="ocologo_footer"
+            width="200"
+            height="200"
+          />
+        </Link>
+        <p className="max-w-xs mt-4 text-sm text-gray-600 mx-auto">
+          We are a web development and design company with a mission to help businesses establish an online presence. We achieve this by continually developing technology, offering expert support, and ensuring a seamless online experience.
+        </p>
+        <div className="flex justify-center space-x-6 text-gray-600 pt-2">
+          {/* Social Media Icons */}
+          <a href="https://www.facebook.com/profile.php?id=100087441512479" target="_blank" rel="noreferrer">
+            <span className="sr-only">Facebook</span>
+            <i className="fab fa-facebook"></i>
+          </a>
+          <a href="https://www.instagram.com/cyber.space.digital/" target="_blank" rel="noreferrer">
+            <span className="sr-only">Instagram</span>
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a href="https://twitter.com/CyberSD1995" target="_blank" rel="noreferrer">
+            <span className="sr-only">Twitter</span>
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a href="https://github.com/akhilthej" target="_blank" rel="noreferrer">
+            <span className="sr-only">GitHub</span>
+            <i className="fab fa-github"></i>
+          </a>
         </div>
-      </section>
+      </div>
+
+      {/* 2nd Column - Empty */}
+      <div></div>
+
+      {/* 3rd Column - Helpful Links */}
+      <div className="text-center">
+        <div>
+          <p className="font-medium text-white">Helpful Links</p>
+          <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+            <Link to="/contactus">Contact</Link>
+            <Link to="/contactus">FAQs</Link>
+            <a href="https://api.whatsapp.com/send?phone=918143407758&text=Welcome%20to%20Cyberspacedigital">Live Chat</a>
+          </nav>
+        </div>
+      </div>
+
+      {/* 4th Column - Legal */}
+      <div className="text-center">
+        <p className="font-medium text-white">Legal</p>
+        <nav className="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+          <Link to="/privacypolicy">Privacy Policy</Link>
+          <Link to="/terms&conditions">Terms &amp; Conditions</Link>
+          <Link to="/returnpolicy">Cancellation & Refund Policy</Link>
+          <Link to="/disclaimer">Disclaimer</Link>
+          <Link to="/shipping&delivery">Shipping & Delivery</Link>
+        </nav>
+      </div>
+    </div>
+
+    {/* Additional Content */}
+    <hr className="h-px my-8 border-0 bg-gray-700" />
+    <p className="cursor-default text-center text-xs text-white">
+      2023 © OTT.in <br />
+      <p className=' text-xs text-gray-600 '>All CopyRights Reserved</p>
+      {/* Designed and Developed by CyberSpaceDigital(www.cyberspacedigital.in) */}
+      <div className="flex pt-5 items-center justify-center">
+        <a href="www.cyberspacedigital.in">
+          <img
+            src={csdlogo_footer}
+            className="h-10 sm:h-10"
+            alt="ocologo_footer"
+            width="200"
+            height="200"
+          />
+        </a>
+      </div>
+    </p>
+  </div>
+</section>
+
     </footer>
   );
 };
